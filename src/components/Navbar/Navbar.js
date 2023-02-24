3e34551e6fb7b2fee07dae202931e89410a2ec98
Crystal <crystalball2000@gmail.com>
2023-02-23 23:38:00 -0500

    add stuff to deploy website

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            Python for ChemE's <i class="fa-brands fa-python"></i>
+            <i class="fa-brands fa-python"></i> for ChemE
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
