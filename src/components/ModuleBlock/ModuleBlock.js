02e485403b8cae3addbc01f88cda7ae8352f57a9
Crystal <crystalball2000@gmail.com>
2023-04-11 04:46:55 -0400

    changed module block to be real component of just each block, and modules page as the HTML of the page in the page folder

--- src/components/ModuleBlock/ModuleBlock.js
+++ src/components/ModuleBlock/ModuleBlock.js
@@ -1,215 +1,22 @@
 import React from "react";
-import { Button } from "../Buttons/Button";
 import "./ModuleBlock.css";
-import GoToTop from "../GoToTop";
-import ButtonBlock from "./Block";
+import { Button } from "../Buttons/Button";
 
-function ModuleBlock() {
+function ModuleBlock({ blockData, title }) {
   return (
-    <>
-      <div className="modules">
-        <div className="title-block">
-          <i className="fa-brands fa-python"></i>
-          <h3>Overall Road Map</h3>
-        </div>
-        <div className="description-block">
-          {/* 1st Section */}
-          <h1>Introduction</h1>
-          <p>
-            Outlined below are the main topics/themes we will cover as well as
-            the individual skills associated with each topic. The plan for this
-            course is that you start by learning the basics an fundamentals of
-            python. As you learn new skills, we will show you examples of how
-            you can apply them, either in an chemE example, or real life data
-            analysis example. Therefore, the philosophy is that while we are
-            building a strong fundamental understanding of python, you can see
-            its usage right after you learn a skill. As we can't teach you
-            everything about python, we hope that this course can teach you new
-            skills and encourage you to continue to learn on your own!
-          </p>
-        </div>
-
-        {/* Block 1 */}
-        <div>
-          <ButtonBlock
-            title="Setting Up Your Workspace"
-            blockData={[
-              [
-                "Visual Studio Code(IDE) and Git bash (command line)",
-                "fa-solid fa-desktop fa-fw",
-                "/modules/setup-workspace",
-              ],
-              [
-                "Downloading Python",
-                "fa-brands fa-python fa-fw",
-                "/modules/installing-python",
-              ],
-              [
-                "Introduction to Jupyter Notebooks",
-                "fa-solid fa-table fa-fw",
-                "/modules/jupyter-notebooks",
-              ],
-            ]}
-          />
-        </div>
-
-        {/* Block 1 */}
-        <div>
-          <div className="module-block module-block-title">
-            Introduction to Python
+    <div className="module-block-container">
+      <div className="module-block module-block-title">{title}</div>
+      <div className="module-block module-block-content">
+        {blockData.map(([topic, logo, path], index) => (
+          <div className="module-block-topic">
+            <Button buttonType="btn--modules" path={path}>
+              <i className={logo}></i>
+              {topic}
+            </Button>
           </div>
-          <div className="module-block module-block-content">
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/modules/python-basics">
-                <i className="fa-brands fa-python fa-fw"></i>Math in Python
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/modules/python-basics">
-                <i className="fa-brands fa-python fa-fw"></i>Data Structures in
-                Python
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/modules/managing-data">
-                <i className="fa-solid fa-table fa-fw"></i>Python Statements
-                (if, for, while, etc)
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/modules/plotting-basics">
-                <i className="fa-solid fa-chart-line fa-fw"></i>Functions in
-                Python
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* Block 1 */}
-        <div>
-          <div className="module-block module-block-title">
-            Working with Data in Python
-          </div>
-          <div className="module-block module-block-content">
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/modules/setup-workspace">
-                <i className="fa-solid fa-desktop fa-fw"></i>Basics of Numpy
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/modules/python-basics">
-                <i className="fa-brands fa-python fa-fw"></i>Basics of Pandas
-                (Dataframes)
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/modules/managing-data">
-                <i className="fa-solid fa-table fa-fw"></i>Importing and
-                Manipulating Data
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/modules/plotting-basics">
-                <i className="fa-solid fa-chart-line fa-fw"></i>Functions in
-                Python
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* Block 2 */}
-        <div>
-          <div className="module-block module-block-title">
-            Basics Applications
-          </div>
-          <div className="module-block module-block-content">
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-solid fa-chart-pie fa-fw"></i>Statistical
-                Analysis for Population
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-solid fa-chart-line fa-fw"></i>Statistical
-                Analysis for Trends
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-solid fa-flask fa-fw"></i>Python Examples for
-                ChemE Classes
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* Block 3 */}
-        <div>
-          <div className="module-block module-block-title">
-            Applications in ChemE
-          </div>
-          <div className="module-block module-block-content">
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-brands fa-python fa-fw"></i>CHBE101: Intro to
-                ChemE
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-brands fa-python fa-fw"></i>CHBE301/302:
-                Thermodynamics
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-brands fa-python fa-fw"></i>CHBE410: Statistics
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-brands fa-python fa-fw"></i>CHBE442: Heat and
-                Mass
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-brands fa-python fa-fw"></i>CHBE437: Unit Ops
-                Lab
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* Block 4 */}
-        <div>
-          <div className="module-block module-block-title">
-            Additional Tools
-          </div>
-          <div className="module-block module-block-content">
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-solid fa-code-branch fa-fw"></i>Git / Version
-                Control
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-brands fa-python fa-fw"></i>Querying databases
-                / SQL
-              </Button>
-            </div>
-            <div className="module-block-topic">
-              <Button buttonType="btn--modules" path="/">
-                <i className="fa-brands fa-python fa-fw"></i>Continue Learning
-              </Button>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
-      <GoToTop />
-    </>
+    </div>
   );
 }
 
--- src/components/pages/Modules.js
+++ src/components/pages/Modules.js
@@ -1,8 +1,162 @@
 import React from "react";
-import "../../App.css";
-import ModuleBlock from "../ModuleBlock/ModuleBlock.js";
-// import Accordion from "../AccordionMenu/Accordion";
+import ModuleBlock from "../ModuleBlock/ModuleBlock";
 
 export default function Modules() {
-  return <ModuleBlock />;
+  return (
+    <>
+      <div className="modules">
+        <div className="title-block">
+          <i className="fa-brands fa-python"></i>
+          <h3>Overall Road Map</h3>
+        </div>
+        <div className="description-block">
+          <h1>Introduction</h1>
+          <p>
+            Outlined below are the main topics/themes we will cover as well as
+            the individual skills associated with each topic. The plan for this
+            course is that you start by learning the basics an fundamentals of
+            python. As you learn new skills, we will show you examples of how
+            you can apply them, either in an chemE example, or real life data
+            analysis example. Therefore, the philosophy is that while we are
+            building a strong fundamental understanding of python, you can see
+            its usage right after you learn a skill. As we can't teach you
+            everything about python, we hope that this course can teach you new
+            skills and encourage you to continue to learn on your own!
+          </p>
+        </div>
+
+        {/* Block 1 */}
+        <ModuleBlock
+          title="Setting Up Your Workspace"
+          blockData={[
+            [
+              "Visual Studio Code (IDE) and Git bash (command line)",
+              "fa-solid fa-desktop fa-fw",
+              "/modules/setup-workspace",
+            ],
+            [
+              "Downloading Python",
+              "fa-brands fa-python fa-fw",
+              "/modules/installing-python",
+            ],
+            [
+              "Introduction to Jupyter Notebooks",
+              "fa-solid fa-table fa-fw",
+              "/modules/jupyter-notebooks",
+            ],
+          ]}
+        />
+
+        {/* Block 2 */}
+        <ModuleBlock
+          title="Introduction to Python"
+          blockData={[
+            [
+              "Math in Python",
+              "fa-brands fa-python fa-fw",
+              "/modules/python-basics",
+            ],
+            [
+              "Data Structures in Python",
+              "fa-brands fa-python fa-fw",
+              "/modules/python-basics",
+            ],
+            [
+              "Python Statements (if, for, while, etc)",
+              "fa-solid fa-table fa-fw",
+              "/modules/managing-data",
+            ],
+            [
+              "Functions in Python",
+              "fa-solid fa-chart-line fa-fw",
+              "/modules/plotting-basics",
+            ],
+          ]}
+        />
+
+        {/* Block 3 */}
+        <ModuleBlock
+          title="Working with Data in Python"
+          blockData={[
+            [
+              "Basics of Numpy",
+              "fa-solid fa-desktop fa-fw",
+              "/modules/setup-workspace",
+            ],
+            [
+              "Basics of Pandas (Dataframes)",
+              "fa-brands fa-python fa-fw",
+              "/modules/python-basics",
+            ],
+            [
+              "Importing and Manipulating Data",
+              "fa-solid fa-table fa-fw",
+              "/modules/managing-data",
+            ],
+            [
+              "Functions in Python",
+              "fa-solid fa-chart-line fa-fw",
+              "/modules/plotting-basics",
+            ],
+          ]}
+        />
+
+        {/* Block 4 */}
+        <ModuleBlock
+          title="Basics Applications"
+          blockData={[
+            [
+              "Statistical Analysis for Population",
+              "fa-solid fa-chart-line fa-fw",
+              "/path/path",
+            ],
+            ["Statistical Analysis for Trends", "fa-solid fa-table fa-fw", "/"],
+            [
+              "Python Examples for ChemE Classes",
+              "fa-solid fa-flask fa-fw",
+              "/",
+            ],
+          ]}
+        />
+
+        {/* Block 5 */}
+        <ModuleBlock
+          title="Applications in ChemE"
+          blockData={[
+            [
+              "CHBE101: Intro to ChemE",
+              "fa-solid fa-chart-pie fa-fw",
+              "/path/path",
+            ],
+            [
+              "CHBE301/302:Thermodynamics",
+              "fa-solid fa-chart-line fa-fw",
+              "/path/path",
+            ],
+            ["CHBE410: Statistics", "fa-solid fa-table fa-fw", "/"],
+            ["CHBE442: Heat and Mass", "fa-solid fa-flask fa-fw", "/"],
+            ["CHBE437: Unit Ops Lab", "fa-solid fa-flask fa-fw", "/"],
+          ]}
+        />
+
+        {/* Block 6 */}
+        <ModuleBlock
+          title="Additional Tools"
+          blockData={[
+            [
+              "Git / Version Control",
+              "fa-solid fa-code-branch fa-fw",
+              "/path/path",
+            ],
+            [
+              "Querying databases / SQL",
+              "fa-brands fa-python fa-fw",
+              "/path/path",
+            ],
+            ["Continue Learning", "fa-brands fa-python fa-fw", "/"],
+          ]}
+        />
+      </div>
+    </>
+  );
 }
